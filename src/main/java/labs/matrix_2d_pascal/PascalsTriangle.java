d4fcae4238dca10fdb845a80e5cbb3273cbddf6d
harry.chen <harryxtchen@gmail.com>
2023-02-10 11:43:10 -0500

    Add PascalsTriangle lab, still have the new TicTacToe lab to do...

--- src/main/java/labs/matrix_2d_pascal/PascalsTriangle.java
+++ src/main/java/labs/matrix_2d_pascal/PascalsTriangle.java
@@ -1,5 +1,24 @@
 package labs.matrix_2d_pascal;
 
 public class PascalsTriangle {
-    //TODO: actually do this
+    public static int factorial(final int i) {
+        if (i == 0)
+            return 1;
+        return i * factorial(i - 1);
+    }
+
+    public static String generate(final int size) {
+        final StringBuilder builder = new StringBuilder();
+        for (int i = 0; i <= size; i++) {
+            builder.append(" ".repeat(Math.max(0, size - i + 1)));
+
+            for (int j = 0; j <= i; j++) {
+                builder.append(" ").append(factorial(i) / (factorial(i - j) * factorial(j)));
+            }
+
+            builder.append("\n");
+        }
+
+        return builder.toString();
+    }
 }
--- src/main/java/labs/matrix_2d_pascal/PascalsTriangleRunner.java
+++ src/main/java/labs/matrix_2d_pascal/PascalsTriangleRunner.java
@@ -1,5 +1,12 @@
 package labs.matrix_2d_pascal;
 
+import java.util.Scanner;
+
 public class PascalsTriangleRunner {
-    //TODO: actually do this
+    final static Scanner inputScanner = new Scanner(System.in);
+    public static void main(final String[] args) {
+        System.out.println("Input Number of Rows for Pascal's Triangle ");
+        final int triangleSize = inputScanner.nextInt();
+        System.out.println(PascalsTriangle.generate(triangleSize - 1));
+    }
 }
